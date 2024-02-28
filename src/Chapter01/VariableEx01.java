2045d49d9433a16e89d4dcec4dbc338fddc5edb5
zingzing <owj20127@gmail.com>
2024-02-28 19:46:05 +0900

    변수의 기초(1)

---
+++ src/Chapter01/VariableEx01.java
@@ -0,0 +1,14 @@
+package Chapter01;
+
+public class VariableEx01 {
+    public static void main(String[] args) {
+        int year;
+
+        // 변수 선언 후 초기화 없이 사용할 경우 에러 발생
+        //System.out.println(year);
+
+        year = 2024;
+
+        System.out.println(year);
+    }
+}
---
+++ src/Chapter01/VariableEx02.java
@@ -0,0 +1,31 @@
+package Chapter01;
+
+public class VariableEx02 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 1;
+        b = 1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        int c, d;
+        c = 1;
+        d = 1;
+
+        System.out.println(c);
+        System.out.println(d);
+
+        int e = 1;
+        int f = 1;
+
+        System.out.println(e);
+        System.out.println(f);
+
+        int g = 1, h = 1;
+
+        System.out.println(g);
+        System.out.println(h);
+    }
+}

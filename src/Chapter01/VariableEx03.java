44f2e43f7308dbbbb59cb4d21f8631697f407b56
zingzing <owj20127@gmail.com>
2024-02-28 22:48:04 +0900

    변수의 기초(2)

--- src/Chapter01/VariableEx03.java
+++ src/Chapter01/VariableEx03.java
@@ -2,10 +2,37 @@ package Chapter01;
 
 public class VariableEx03 {
     public static void main(String[] args) {
-        final double PI = 3.14159;
-        final int DAYS_IN_WEEK = 7;
+        /* 논리형 */
+        boolean flag = true;
 
-        System.out.println(PI);
-        System.out.println(DAYS_IN_WEEK);
+        System.out.println("=======논리형=======");
+        System.out.println(flag);
+
+        /* 문자형 */
+        char a = 'A';
+
+        System.out.println("=======문자형=======");
+        System.out.println(a);
+        System.out.println((int) a);	// A 문자의 유니코드 정수 출력
+
+        /* 정수형 */
+        byte b = 127;
+        short s = 32_767;
+        int i = 32_768;
+        long l = 7_852_354_812L;
+
+        System.out.println("=======정수형=======");
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+
+        /* 실수형 */
+        float f = 3.141592F;
+        double d = 3.141592653589793;
+
+        System.out.println("=======실수형=======");
+        System.out.println(f);
+        System.out.println(d);
     }
 }
---
+++ src/Chapter01/VariableEx04.java
@@ -0,0 +1,11 @@
+package Chapter01;
+
+public class VariableEx04 {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        final int DAYS_IN_WEEK = 7;
+
+        System.out.println(PI);
+        System.out.println(DAYS_IN_WEEK);
+    }
+}

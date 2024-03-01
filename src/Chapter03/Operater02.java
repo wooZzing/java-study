9988a9643169294920906e3b78ef57ebed25b52c
zingzing <owj20127@gmail.com>
2024-03-01 22:55:56 +0900

    논리연산자 추가

--- src/Chapter03/Operater02.java
+++ src/Chapter03/Operater02.java
@@ -2,11 +2,11 @@ package Chapter03;
 
 public class Operater02 {
     public static void main(String[] args) {
-        int c = 10, d = 20;
+        int a = 10, b = 20;
 
-        System.out.println("c == d: " + (c == d)); // 같음
-        System.out.println("c != d: " + (c != d)); // 다름
-        System.out.println("c > d: " + (c > d)); // 큼
-        System.out.println("c < d: " + (c < d)); // 작음
+        System.out.println("c == d: " + (a == b)); // 같음
+        System.out.println("c != d: " + (a != b)); // 다름
+        System.out.println("c > d: " + (a > b)); // 큼
+        System.out.println("c < d: " + (a < b)); // 작음
     }
 }
---
+++ src/Chapter03/Operater03.java
@@ -0,0 +1,11 @@
+package Chapter03;
+
+public class Operater03 {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+
+        System.out.println("a && b: " + (a && b)); // AND
+        System.out.println("a || b: " + (a || b)); // OR
+        System.out.println("!a: " + (!a)); // NOT
+    }
+}

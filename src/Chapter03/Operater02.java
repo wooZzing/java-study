1b40593414a4416ac30108763c1e6a2e2dda8b9b
zingzing <owj20127@gmail.com>
2024-03-01 22:53:34 +0900

    비교연산자 추가

---
+++ src/Chapter03/Operater02.java
@@ -0,0 +1,12 @@
+package Chapter03;
+
+public class Operater02 {
+    public static void main(String[] args) {
+        int c = 10, d = 20;
+
+        System.out.println("c == d: " + (c == d)); // 같음
+        System.out.println("c != d: " + (c != d)); // 다름
+        System.out.println("c > d: " + (c > d)); // 큼
+        System.out.println("c < d: " + (c < d)); // 작음
+    }
+}

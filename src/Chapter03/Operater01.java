0113121fe5212d5b74fca487c24fcbd389cca8de
zingzing <owj20127@gmail.com>
2024-03-01 22:47:29 +0900

    산술연산자 추가

---
+++ src/Chapter03/Operater01.java
@@ -0,0 +1,13 @@
+package Chapter03;
+
+public class Operater01 {
+    public static void main(String[] args) {
+        int a = 12, b = 5;
+
+        System.out.println("a + b = " + (a + b)); // 덧셈
+        System.out.println("a - b = " + (a - b)); // 뺄셈
+        System.out.println("a * b = " + (a * b)); // 곱셈
+        System.out.println("a / b = " + (a / b)); // 나눗셈
+        System.out.println("a % b = " + (a % b)); // 나머지
+    }
+}

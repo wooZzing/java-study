74968c685e7169981769303f63044ffc7c1d66d5
zingzing <owj20127@gmail.com>
2024-03-02 23:49:43 +0900

    단항연산자 추가

--- src/Chapter03/Operater01.java
+++ src/Chapter03/Operater01.java
@@ -2,12 +2,35 @@ package Chapter03;
 
 public class Operater01 {
     public static void main(String[] args) {
-        int a = 12, b = 5;
+        int a = 5, b = -10;
 
-        System.out.println("a + b = " + (a + b)); // 덧셈
-        System.out.println("a - b = " + (a - b)); // 뺄셈
-        System.out.println("a * b = " + (a * b)); // 곱셈
-        System.out.println("a / b = " + (a / b)); // 나눗셈
-        System.out.println("a % b = " + (a % b)); // 나머지
+        System.out.print("a = " + a);
+        System.out.print(", b = " + b);
+        System.out.println();
+
+        // 단항 플러스 연산자: 변수의 값을 변경하지 않음
+        System.out.println("+a: " + (+a)); // 5
+
+        // 단항 마이너스 연산자: 변수의 부호를 반전시킴
+        System.out.println("-a: " + (-a)); // -5
+        System.out.println("-b: " + (-b)); // 10
+
+        // 전위 증가 연산자: 변수의 값을 1 증가시킴
+        System.out.println("++a 전: " + a); // 5
+        System.out.println("++a 후: " + (++a)); // 6
+
+        // 전위 감소 연산자: 변수의 값을 1 감소시킴
+        System.out.println("--a 전: " + a); // 6
+        System.out.println("--a 후: " + (--a)); // 5
+
+        // 후위 증가 연산자: 표현식 평가 후 변수의 값을 1 증가시킴
+        System.out.println("a++ 전: " + a); // 5
+        System.out.println("a++ 중: " + (a++)); // 5
+        System.out.println("a++ 후: " + a); // 6
+
+        // 후위 감소 연산자: 표현식 평가 후 변수의 값을 1 감소시킴
+        System.out.println("a-- 전: " + a); // 6
+        System.out.println("a-- 중: " + (a--)); // 6
+        System.out.println("a-- 후: " + a); // 5
     }
 }

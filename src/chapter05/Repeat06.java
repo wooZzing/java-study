aaefde090ec1ab089048891b9d6509ccbee8affe
zingzing <owj20127@gmail.com>
2024-03-08 21:02:45 +0900

    while문 실습 코드 추가

--- src/chapter05/Repeat06.java
+++ src/chapter05/Repeat06.java
@@ -2,6 +2,7 @@ package chapter05;
 
 public class Repeat06 {
     public static void main(String[] args) {
+        System.out.println("==for 반복문==");
         int sum = 0;    // 합계를 저장 할 변수를 0으로 초기화 합니다.
 
         // 1부터 10까지 반복문을 실행합니다.
@@ -10,5 +11,18 @@ public class Repeat06 {
         }
 
         System.out.println(sum);    // 합계를 출력합니다.
+
+        System.out.println("==while 반복문==");
+        int sum2 = 0;// 합계를 저장 할 변수를 0으로 초기화 합니다.
+
+        int i = 1; // 반복문에 사용할 카운트 변수 i를 초기화 합니다.
+
+        // 1부터 10까지 합계를 구합니다.
+        while (i <= 10) {
+            sum2 += i;   // sum 변수에 i의 값을 더합니다.
+            i++; // i의 값을 1 증가시킵니다.
+        }
+
+        System.out.print(sum2); // sum의 값을 출력합니다.
     }
 }
